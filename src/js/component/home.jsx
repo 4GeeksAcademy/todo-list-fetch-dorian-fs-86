0cc5c8ddb0bc3ec46aa45fdebf78f292e3bbf99b
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2024-12-19 08:23:16 +0000

    todo-list-fetch-dorian

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [state, setState] = useState([]);
+	const [input, setInput] = useState('');
+
+	useEffect(() => {
+		getDataUser()
+	}, []);
+
+
+	const getDataUser = async () => {
+		const response = await fetch(
+			"https://playground.4geeks.com/todo/users/dorian",
+			{ method: 'GET' }
+		);
+		if(!response.ok ){
+			createUser();
+		}
+		const data = await response.json();
+		setState(data.todos)
+		console.log(data)
+	};
+
+	const createUser = async () => {
+		const response = await fetch("https://playground.4geeks.com/todo/users/dorian", { method: "POST"} );
+		return await response.json();
+	}
+
+
+	const add = async () => {
+		if(input.trim() === "") return; 
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/todos/dorian",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					"label": input,
+					"is_done": false
+				  }),
+				headers: {"content-type" : "application/json"}
+			}
+		);
+		const newTask = await response.json();
+		console.log("this is your new task", newTask);
+		setState([...state, newTask]);
+		setInput('');
+
+		} catch (error) {
+			console.error("error", error);
+		}
+	};
+
+	const deleteTask = async (id) => {
+		const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {method: "DELETE"});
+		const filtered = state.filter((item) => {
+			return item.id !== id;
+		});
+		setState(filtered);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo-list">
+			<h1>TODO LIST</h1>
+			<div className="input-add">
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					type="text"
+					placeholder="Add List"
+				></input>
+				<button onClick={add}>+</button>
+			</div>
+			<ul>
+			{state.length <= 0
+					? 'No Tasks'
+					: state.map((item) => {
+						return (
+							<div>
+								<li>
+									{item.label}
+									<button className="delete"
+										onClick={() => {
+											deleteTask(item.id);
+										}}
+									>
+										x
+									</button>
+								</li>
+							</div>
+						);
+					})}
+			</ul>
 		</div>
 	);
 };
